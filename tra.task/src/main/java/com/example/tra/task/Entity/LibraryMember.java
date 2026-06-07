6687b7c9d7c5a526f8e3bd291d9b2172ac039c11
Muneera Al Naabi <muneerasaifalnaabi@gmail.com>
2026-06-07 14:46:58 +0400

    added lombok annotation

--- tra.task/src/main/java/com/example/tra/task/Entity/LibraryMember.java
+++ tra.task/src/main/java/com/example/tra/task/Entity/LibraryMember.java
@@ -1,5 +1,12 @@
 package com.example.tra.task.Entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class LibraryMember {
     private  String memberId;
     private String fullName;
