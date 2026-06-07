44e47f302b6cb82458326c81fbf60f08b9da8381
Muneera Al Naabi <muneerasaifalnaabi@gmail.com>
2026-06-07 10:07:59 +0400

    added updateStudent Method

--- tra.task/src/main/java/com/example/tra/task/StudentController.java
+++ tra.task/src/main/java/com/example/tra/task/StudentController.java
@@ -1,7 +1,5 @@
 package com.example.tra.task;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
