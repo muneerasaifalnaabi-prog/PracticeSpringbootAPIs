3c80c6d1d96d7c09add17d87a462214a6c67cad3
Muneera Al Naabi <muneerasaifalnaabi@gmail.com>
2026-06-07 16:11:33 +0400

    create registrationservice object

--- tra.task/src/main/java/com/example/tra/task/Controller/RegistrationController.java
+++ tra.task/src/main/java/com/example/tra/task/Controller/RegistrationController.java
@@ -1,4 +1,14 @@
 package com.example.tra.task.Controller;
 
+import com.example.tra.task.Services.RegistrationService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/registrations")
 public class RegistrationController {
+    @Autowired
+    RegistrationService registrationService;
+
 }
