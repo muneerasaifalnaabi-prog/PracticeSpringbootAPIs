385df583299e4635e6f40f977a93ed764a871e72
Muneera Al Naabi <muneerasaifalnaabi@gmail.com>
2026-06-07 15:31:42 +0400

    added productService object

--- tra.task/src/main/java/com/example/tra/task/Controller/ProductsController.java
+++ tra.task/src/main/java/com/example/tra/task/Controller/ProductsController.java
@@ -1,7 +1,15 @@
 package com.example.tra.task.Controller;
 
+import com.example.tra.task.Entity.Product1;
+import com.example.tra.task.Services.ProductService;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/products")
 public class ProductsController {
+
+    ProductService productService;
+
+
 }
