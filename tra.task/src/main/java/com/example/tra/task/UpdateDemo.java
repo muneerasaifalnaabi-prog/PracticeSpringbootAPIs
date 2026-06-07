77477ceb874bbf1d85bc2db3c460346b6b384e28
Muneera Al Naabi <muneerasaifalnaabi@gmail.com>
2026-06-07 10:19:19 +0400

    added Student Object class

--- tra.task/src/main/java/com/example/tra/task/UpdateDemo.java
+++ tra.task/src/main/java/com/example/tra/task/UpdateDemo.java
@@ -3,7 +3,7 @@ package com.example.tra.task;
 public class UpdateDemo {
     public static void main(String[] args) {
 
-
+/*  Task 26
         String currentName = "Ahmed";
         String newName = "Ali";
         String status ;
@@ -25,7 +25,10 @@ public class UpdateDemo {
 
         System.out.println("Current Student Name :" + currentName);
         System.out.println("updated status :" +status);
-
+*/
     }
+    Student student = new Student("101","Ahmed","A");
+
+
 
 }
