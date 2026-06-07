ee137f1433724d832889cb6b96307d522fe9f8eb
Muneera Al Naabi <muneerasaifalnaabi@gmail.com>
2026-06-07 16:04:06 +0400

    Added deleteMultipleByIds with 2 list

--- tra.task/src/main/java/com/example/tra/task/Services/RegistrationService.java
+++ tra.task/src/main/java/com/example/tra/task/Services/RegistrationService.java
@@ -2,7 +2,9 @@ package com.example.tra.task.Services;
 
 import com.example.tra.task.Entity.EventRegistration;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class RegistrationService {
@@ -17,5 +19,12 @@ public class RegistrationService {
         registrations.put(103, new EventRegistration(103, "John"));
         registrations.put(104, new EventRegistration(104, "Ali"));
     }
+    public Map<String, Object> deleteMultipleByIds(List<Integer> ids) {
+        List<Integer> deletedIds = new ArrayList<>();
+        List<Integer> notFoundIds = new ArrayList<>();
+
+
+    }
+
 
 }
