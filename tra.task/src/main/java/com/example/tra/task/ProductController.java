487c756114dc8a9b23e268b02fef3e950ddf8593
Muneera Al Naabi <muneerasaifalnaabi@gmail.com>
2026-06-07 12:20:08 +0400

    Check if product exists

--- tra.task/src/main/java/com/example/tra/task/ProductController.java
+++ tra.task/src/main/java/com/example/tra/task/ProductController.java
@@ -2,7 +2,9 @@ package com.example.tra.task;
 
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -12,9 +14,13 @@ public class ProductController {
     Map<Integer, Product> productData = new HashMap<>();
 
     @PutMapping("/updateStock/{id}")
-    public String updateStock(){
+    public String updateStock(@PathVariable String id , @RequestParam Integer newStock){
         productData.put(1,new Product("1","Laptop",10));
         productData.put(1,new Product("2","Phone",20));
+
+        if (productData.containsKey(id)){
+            return "Product not found"
+        }
 
     }
 
