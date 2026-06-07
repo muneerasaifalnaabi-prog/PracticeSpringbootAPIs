227ceb580d5a8758612900ed1b6e570d8b251fbd
Muneera Al Naabi <muneerasaifalnaabi@gmail.com>
2026-06-07 15:33:35 +0400

    added deleteProduct method

--- tra.task/src/main/java/com/example/tra/task/Controller/ProductsController.java
+++ tra.task/src/main/java/com/example/tra/task/Controller/ProductsController.java
@@ -2,6 +2,8 @@ package com.example.tra.task.Controller;
 
 import com.example.tra.task.Entity.Product1;
 import com.example.tra.task.Services.ProductService;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,5 +13,9 @@ public class ProductsController {
 
     ProductService productService;
 
+   @DeleteMapping("/{id}")
+    public String deleteProduct(@PathVariable int id){
+
+   }
 
 }
