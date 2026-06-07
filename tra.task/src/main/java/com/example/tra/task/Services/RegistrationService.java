98e06d5602c09aa3c6c5f04e2a0685ed30cb3cea
Muneera Al Naabi <muneerasaifalnaabi@gmail.com>
2026-06-07 15:54:07 +0400

    Added registration collection

--- tra.task/src/main/java/com/example/tra/task/Services/RegistrationService.java
+++ tra.task/src/main/java/com/example/tra/task/Services/RegistrationService.java
@@ -1,4 +1,10 @@
 package com.example.tra.task.Services;
 
+import com.example.tra.task.Entity.EventRegistration;
+
+import java.util.HashMap;
+import java.util.Map;
+
 public class RegistrationService {
+    Map<Integer, EventRegistration> registrations = new HashMap<>();
 }
