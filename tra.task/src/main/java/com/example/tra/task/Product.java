a1001ec42fc4bc63dd4b30915ecde6fd9431774c
Muneera Al Naabi <muneerasaifalnaabi@gmail.com>
2026-06-07 12:00:50 +0400

    Added lombok Annotation

--- tra.task/src/main/java/com/example/tra/task/Product.java
+++ tra.task/src/main/java/com/example/tra/task/Product.java
@@ -1,5 +1,12 @@
 package com.example.tra.task;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Product {
     private  String pid;
     private  String productName;
