f016ab7945aaccca2b75ac0dd96b604e865f7aa8
Muneera Al Naabi <muneerasaifalnaabi@gmail.com>
2026-06-07 12:33:05 +0400

    added Annotation

--- tra.task/src/main/java/com/example/tra/task/Order.java
+++ tra.task/src/main/java/com/example/tra/task/Order.java
@@ -1,5 +1,12 @@
 package com.example.tra.task;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Order {
     private  String orederId ;
     private String shippingAddress;
