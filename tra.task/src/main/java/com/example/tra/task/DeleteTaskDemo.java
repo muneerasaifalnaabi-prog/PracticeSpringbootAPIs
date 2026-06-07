30f4979122f737685b953c5ff34c4546e12d0fd0
Muneera Al Naabi <muneerasaifalnaabi@gmail.com>
2026-06-07 14:29:50 +0400

    added task collection using arraylist

--- tra.task/src/main/java/com/example/tra/task/DeleteTaskDemo.java
+++ tra.task/src/main/java/com/example/tra/task/DeleteTaskDemo.java
@@ -1,4 +1,13 @@
 package com.example.tra.task;
 
+import java.util.ArrayList;
+
 public class DeleteTaskDemo {
+    public static void main(String[] args) {
+
+
+        ArrayList<String> task = new ArrayList<>();
+
+
+    }
 }
