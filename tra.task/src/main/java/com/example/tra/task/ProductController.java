3f08669726ca131cb4a7dcc89ea16c528ee8217d
Muneera Al Naabi <muneerasaifalnaabi@gmail.com>
2026-06-07 12:17:27 +0400

    Added methood updateStock

--- tra.task/src/main/java/com/example/tra/task/ProductController.java
+++ tra.task/src/main/java/com/example/tra/task/ProductController.java
@@ -2,6 +2,7 @@ package com.example.tra.task;
 
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.web.bind.annotation.PutMapping;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -10,10 +11,12 @@ import java.util.Map;
 public class ProductController {
     Map<Integer, Product> productData = new HashMap<>();
 
+    @PutMapping("/updateStock/{id}")
     public String updateStock(){
-        productData.put(1,new Product(1,"Laptop",))
-    }
+        productData.put(1,new Product("1","Laptop",10));
+        productData.put(1,new Product("2","Phone",20));
 
+    }
 
 
 
