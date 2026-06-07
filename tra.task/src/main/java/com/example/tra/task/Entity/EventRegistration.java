ff222af2527d621b42ae054aeb15ed455c715616
Muneera Al Naabi <muneerasaifalnaabi@gmail.com>
2026-06-07 15:53:00 +0400

    Added Lombok annotation

--- tra.task/src/main/java/com/example/tra/task/Entity/EventRegistration.java
+++ tra.task/src/main/java/com/example/tra/task/Entity/EventRegistration.java
@@ -1,5 +1,12 @@
 package com.example.tra.task.Entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class EventRegistration {
     private  Integer registrationId ;
     private  String attendeeName;
