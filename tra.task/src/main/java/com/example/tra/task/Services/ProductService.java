3101a749cff867af886862cbcc2be308508628be
Muneera Al Naabi <muneerasaifalnaabi@gmail.com>
2026-06-07 15:22:53 +0400

    added data in loadSampleProducts() method

--- tra.task/src/main/java/com/example/tra/task/Services/ProductService.java
+++ tra.task/src/main/java/com/example/tra/task/Services/ProductService.java
@@ -8,6 +8,9 @@ public class ProductService {
     HashMap<Integer, Product> productMap = new HashMap<>();
 
     public void oadSampleProducts() {
-
+        productMap.put(1, new Product(1, "Laptop", 500));
+        productMap.put(2, new Product(2, "Phone", 300));
+        productMap.put(3, new Product(3, "Tablet", 200));
     }
+
 }
