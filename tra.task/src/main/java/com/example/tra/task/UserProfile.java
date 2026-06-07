c0f2106bc62ffbc625fcc27138cd780bb6d29dee
Muneera Al Naabi <muneerasaifalnaabi@gmail.com>
2026-06-07 11:40:56 +0400

    Added lombok Annotation

--- tra.task/src/main/java/com/example/tra/task/UserProfile.java
+++ tra.task/src/main/java/com/example/tra/task/UserProfile.java
@@ -1,8 +1,15 @@
 package com.example.tra.task;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class UserProfile {
     private String userId;
-
-
+    private String phoneNumber;
+    private String address;
 
 }
