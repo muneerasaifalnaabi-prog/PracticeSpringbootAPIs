2b58c026e60a89862fe8270d18679dbccc5cf7fb
Muneera Al Naabi <muneerasaifalnaabi@gmail.com>
2026-06-07 12:41:28 +0400

    added Map to store order data

--- tra.task/src/main/java/com/example/tra/task/OrderController.java
+++ tra.task/src/main/java/com/example/tra/task/OrderController.java
@@ -1,4 +1,13 @@
 package com.example.tra.task;
 
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
 public class OrderController {
+    Map<Integer,Order> orderData =new HashMap<>();
+
+
 }
--- tra.task/src/main/java/com/example/tra/task/ProductController.java
+++ tra.task/src/main/java/com/example/tra/task/ProductController.java
@@ -1,5 +1,6 @@
 package com.example.tra.task;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -9,14 +10,15 @@ import org.springframework.web.bind.annotation.RequestParam;
 import java.util.HashMap;
 import java.util.Map;
 
-
+@NoArgsConstructor
+@Data
+@AllArgsConstructor
 public class ProductController {
     Map<Integer, Product> productData = new HashMap<>();
 
     @PutMapping("/updateStock/{id}")
     public String updateStock(@PathVariable String id , @RequestParam Integer newStock){
-        productData.put(1,new Product("1","Laptop",10));
-        productData.put(1,new Product("2","Phone",20));
+
 
         if (!productData.containsKey(id)){
             return "Product not found";
