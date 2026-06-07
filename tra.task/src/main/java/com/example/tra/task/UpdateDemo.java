08cade7eb5f3aafccc6691f26f04cd47bc6ae5f4
Muneera Al Naabi <muneerasaifalnaabi@gmail.com>
2026-06-07 09:35:31 +0400

    added name fields and status

--- tra.task/src/main/java/com/example/tra/task/UpdateDemo.java
+++ tra.task/src/main/java/com/example/tra/task/UpdateDemo.java
@@ -1,4 +1,15 @@
 package com.example.tra.task;
 
 public class UpdateDemo {
+    public static void main(String[] args) {
+
+
+        String name = "Muneera";
+        String newName = "Reem";
+        Boolean status = false;
+
+        System.out.println("Current Student Name :" +name);
+
+    }
+
 }
