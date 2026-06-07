8bd59a22848f8156c6a423f4e7347b20f4eb80c1
Muneera Al Naabi <muneerasaifalnaabi@gmail.com>
2026-06-07 11:30:48 +0400

    Add updateStatus field

--- tra.task/src/main/java/com/example/tra/task/UpdateEmailDemo.java
+++ tra.task/src/main/java/com/example/tra/task/UpdateEmailDemo.java
@@ -3,4 +3,5 @@ package com.example.tra.task;
 public class UpdateEmailDemo {
     String currentEmail = "ahmed@gmail.com";
     String newEmail ="ali@gmail.com";
+    boolean updateStatus =false;
 }
