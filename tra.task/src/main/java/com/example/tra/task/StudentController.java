7e45c78bb023a4b2a8327c15e3525a22c8daaa89
Muneera Al Naabi <muneerasaifalnaabi@gmail.com>
2026-06-07 10:02:43 +0400

    added Object class

--- tra.task/src/main/java/com/example/tra/task/StudentController.java
+++ tra.task/src/main/java/com/example/tra/task/StudentController.java
@@ -1,7 +1,14 @@
 package com.example.tra.task;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+
 public class StudentController {
+    @Autowired
+    Student student = new Student();
+
+    @GetMapping("/updateStudent ")
 }
