5247d179b5a222aaae7835e01e0007ff1cd23cf2
Muneera Al Naabi <muneerasaifalnaabi@gmail.com>
2026-06-07 15:07:35 +0400

    Create object for librarymanger

--- tra.task/src/main/java/com/example/tra/task/LibraryDemo.java
+++ tra.task/src/main/java/com/example/tra/task/LibraryDemo.java
@@ -1,7 +1,11 @@
 package com.example.tra.task;
 
+import com.example.tra.task.Entity.LibraryManager;
+
 public class LibraryDemo {
     public static void main(String[] args) {
+
+        LibraryManager manager = new LibraryManager();
 
 
     }
