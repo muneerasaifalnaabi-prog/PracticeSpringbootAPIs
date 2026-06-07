8f470c69fb0b6df88b00950ecb9366788f64aeca
Muneera Al Naabi <muneerasaifalnaabi@gmail.com>
2026-06-07 15:55:50 +0400

    Added loadSampleData with data

--- tra.task/src/main/java/com/example/tra/task/Services/RegistrationService.java
+++ tra.task/src/main/java/com/example/tra/task/Services/RegistrationService.java
@@ -7,4 +7,15 @@ import java.util.Map;
 
 public class RegistrationService {
     Map<Integer, EventRegistration> registrations = new HashMap<>();
+
+    public RegistrationService(){
+        loadSampleData();
+    }
+    public void loadSampleData() {
+        registrations.put(101, new EventRegistration(101, "Ahmed"));
+        registrations.put(102, new EventRegistration(102, "Sara"));
+        registrations.put(103, new EventRegistration(103, "John"));
+        registrations.put(104, new EventRegistration(104, "Ali"));
+    }
+
 }
