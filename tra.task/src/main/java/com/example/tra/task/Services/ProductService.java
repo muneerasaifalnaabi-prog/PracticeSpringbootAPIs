52b2ef6965aac554d109ee00412acfc677d6a0d1
Muneera Al Naabi <muneerasaifalnaabi@gmail.com>
2026-06-07 15:24:05 +0400

    added deleteProductById method

--- tra.task/src/main/java/com/example/tra/task/Services/ProductService.java
+++ tra.task/src/main/java/com/example/tra/task/Services/ProductService.java
@@ -7,10 +7,15 @@ import java.util.HashMap;
 public class ProductService {
     HashMap<Integer, Product> productMap = new HashMap<>();
 
-    public void oadSampleProducts() {
+    public void loadSampleProducts() {
         productMap.put(1, new Product(1, "Laptop", 500));
         productMap.put(2, new Product(2, "Phone", 300));
         productMap.put(3, new Product(3, "Tablet", 200));
     }
+
+    public  String deleteProductById(Integer pid){
+
+    }
+
 
 }
