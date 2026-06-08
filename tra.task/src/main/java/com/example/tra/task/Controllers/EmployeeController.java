3f43efc3322a3a33156160796ef44aa47f9ae334
Muneera Al Naabi <muneerasaifalnaabi@gmail.com>
2026-06-08 10:05:16 +0400

    Display all employee using getAllEmployee method

--- tra.task/src/main/java/com/example/tra/task/Controllers/EmployeeController.java
+++ tra.task/src/main/java/com/example/tra/task/Controllers/EmployeeController.java
@@ -1,4 +1,27 @@
 package com.example.tra.task.Controllers;
 
+import com.example.tra.task.Entities.Employee;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+@RestController
+@RequestMapping("/employees")
 public class EmployeeController {
+
+    List<Employee> employees = new ArrayList<>();
+
+
+    @GetMapping
+    public List<Employee> getAllEmployee() {
+
+        employees.add(new Employee("E101", "Sara", "HR"));
+        employees.add(new Employee("E102", "Ahmed", "Finance"));
+        employees.add(new Employee("E103", "Ali", "Sales"));
+
+        return employees;
+
+    }
 }
