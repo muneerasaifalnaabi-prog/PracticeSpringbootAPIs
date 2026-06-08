451de9b9e08c215797b2110deef267de842327e2
Muneera Al Naabi <muneerasaifalnaabi@gmail.com>
2026-06-08 10:34:59 +0400

    add a list collection to store vehicles data

--- tra.task/src/main/java/com/example/tra/task/Services/VehicleManager.java
+++ tra.task/src/main/java/com/example/tra/task/Services/VehicleManager.java
@@ -1,7 +1,12 @@
 package com.example.tra.task.Services;
 
+import com.example.tra.task.Entities.Vehicle;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class VehicleManager {
+    List<Vehicle> vehicles = new ArrayList<>();
 }
