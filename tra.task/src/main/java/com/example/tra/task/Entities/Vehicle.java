f743cb359379d703102bc320f19eaf9230eb3f7e
Muneera Al Naabi <muneerasaifalnaabi@gmail.com>
2026-06-08 10:33:01 +0400

    add fields with lombok annotation

--- tra.task/src/main/java/com/example/tra/task/Entities/Vehicle.java
+++ tra.task/src/main/java/com/example/tra/task/Entities/Vehicle.java
@@ -1,4 +1,14 @@
 package com.example.tra.task.Entities;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
 public class Vehicle {
+    private String vehicleId;
+    private String vehicleModel;
+    private double rentalPricePerDay;
 }
