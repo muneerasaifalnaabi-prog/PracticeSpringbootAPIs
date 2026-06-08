4e88615000387f4b6cbeb5577ca61f3cd75498a3
Muneera Al Naabi <muneerasaifalnaabi@gmail.com>
2026-06-08 09:04:50 +0400

    add fields

--- tra.task/src/main/java/com/example/tra/task/Entities/Employee.java
+++ tra.task/src/main/java/com/example/tra/task/Entities/Employee.java
@@ -1,4 +1,15 @@
 package com.example.tra.task.Entities;
 
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Employee {
+    private String empid;
+    private String empName;
+    private String Department;
 }
