ec9a3a977f41cbec85ec042a27a9c6dbcd36e843
Muneera Al Naabi <muneerasaifalnaabi@gmail.com>
2026-06-08 10:48:38 +0400

    add getAllVehicles and addVehicle method to get and add new data

--- tra.task/src/main/java/com/example/tra/task/Controllers/VehicleController.java
+++ tra.task/src/main/java/com/example/tra/task/Controllers/VehicleController.java
@@ -4,9 +4,7 @@ package com.example.tra.task.Controllers;
 import com.example.tra.task.Entities.Vehicle;
 import com.example.tra.task.Services.VehicleManager;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -14,13 +12,19 @@ import java.util.List;
 @RequestMapping("/vehicles")
 public class VehicleController {
     @Autowired
-    VehicleManager  vehicleManager;
+    VehicleManager vehicleManager;
 
     @GetMapping
     public List<Vehicle> getAllVehicles() {
         return vehicleManager.getAllVehicles();
     }
 
-
-
+    @PostMapping
+    public String addVehicle(@RequestBody Vehicle vehicle) {
+        return vehicleManager.addVehicle(vehicle);
+    }
 }
+
+
+
+
