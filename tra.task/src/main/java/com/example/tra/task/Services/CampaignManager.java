2fe6398b30f7ec8e9cafb693a23a6184b72b1fc2
Muneera Al Naabi <muneerasaifalnaabi@gmail.com>
2026-06-08 10:58:31 +0400

    add CampaignManager with getAllCampaigns to display data and addCampaign to add new data

---
+++ tra.task/src/main/java/com/example/tra/task/Services/CampaignManager.java
@@ -0,0 +1,37 @@
+package com.example.tra.task.Services;
+
+import com.example.tra.task.Entities.Campaign;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class CampaignManager {
+    private List<Campaign> campaigns = new ArrayList<>();
+
+    public CampaignManager() {
+        campaigns.add(new Campaign("C101", "Summer Sale", "Instagram", 500));
+        campaigns.add(new Campaign("C102", "Black Friday", "Google Ads", 1000));
+        campaigns.add(new Campaign("C103", "Email Promo", "Email", 300));
+    }
+
+    public List<Campaign> getAllCampaigns() {
+        return campaigns;
+    }
+
+    public String addCampaign(Campaign newCampaign) {
+
+        for (Campaign campaign : campaigns) {
+            if (campaign.getCampaignId().equals(newCampaign.getCampaignId())) {
+                return "Campaign ID already exists. No campaign was created.";
+            }
+        }
+
+        campaigns.add(newCampaign);
+
+        return "Campaign Created Successfully\n" + "Campaign ID: " + newCampaign.getCampaignId() + "\n" + "Campaign Name: " + newCampaign.getCampaignName() + "\n" + "Platform: " + newCampaign.getPlatform() + "\n" + "Budget: " + newCampaign.getBudget();
+    }
+
+
+}
