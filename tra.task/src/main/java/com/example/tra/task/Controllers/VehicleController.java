25fe1b92177aa662395cd50cc633ad703251b53b
Muneera Al Naabi <muneerasaifalnaabi@gmail.com>
2026-06-08 10:47:30 +0400

    add object for VehicleManager

--- tra.task/src/main/java/com/example/tra/task/Controllers/VehicleController.java
+++ tra.task/src/main/java/com/example/tra/task/Controllers/VehicleController.java
@@ -1,16 +1,25 @@
 package com.example.tra.task.Controllers;
 
 
+import com.example.tra.task.Entities.Vehicle;
 import com.example.tra.task.Services.VehicleManager;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/vehicles")
 public class VehicleController {
     @Autowired
     VehicleManager  vehicleManager;
+
+    @GetMapping
+    public List<Vehicle> getAllVehicles() {
+        return vehicleManager.getAllVehicles();
+    }
 
 
 
