e23c4ef0f3f7d9a57d49dbbfb5ec06486eff0660
Muneera Al Naabi <muneerasaifalnaabi@gmail.com>
2026-06-08 10:29:59 +0400

    add Method in controller

--- tra.task/src/main/java/com/example/tra/task/Controllers/EmployeeController.java
+++ tra.task/src/main/java/com/example/tra/task/Controllers/EmployeeController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
+
 @RestController
 @RequestMapping("/employees")
 public class EmployeeController {
@@ -18,8 +19,9 @@ public class EmployeeController {
         return employeeService.getAllEmployee();
 
     }
+
     @PostMapping
-    public String addEmployee(@RequestBody Employee employee){
+    public String addEmployee(@RequestBody Employee employee) {
         return employeeService.addEmployee(employee);
     }
 
