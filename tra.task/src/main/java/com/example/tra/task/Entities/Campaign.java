bef1590afc2c6c25aa16a6ed30b010d8c562cb66
Muneera Al Naabi <muneerasaifalnaabi@gmail.com>
2026-06-08 10:55:26 +0400

    add Campaign with fields

---
+++ tra.task/src/main/java/com/example/tra/task/Entities/Campaign.java
@@ -0,0 +1,13 @@
+package com.example.tra.task.Entities;
+
+
+import lombok.Data;
+
+@Data
+public class Campaign {
+    private String campaignId;
+    private String campaignName;
+    private String platform;
+    private double budget;
+
+}
