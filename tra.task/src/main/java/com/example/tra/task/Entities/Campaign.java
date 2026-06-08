f7b027b49c863219e754e4ad66036168c6c5f415
Muneera Al Naabi <muneerasaifalnaabi@gmail.com>
2026-06-08 10:55:39 +0400

    add Campaign with fields

--- tra.task/src/main/java/com/example/tra/task/Entities/Campaign.java
+++ tra.task/src/main/java/com/example/tra/task/Entities/Campaign.java
@@ -1,9 +1,13 @@
 package com.example.tra.task.Entities;
 
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Campaign {
     private String campaignId;
     private String campaignName;
