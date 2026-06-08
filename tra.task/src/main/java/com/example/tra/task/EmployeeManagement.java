af1d1f70b72d3756b531eecbe4870dc55a7c5f40
Muneera Al Naabi <muneerasaifalnaabi@gmail.com>
2026-06-08 09:09:03 +0400

    create collection of employee

--- tra.task/src/main/java/com/example/tra/task/EmployeeManagement.java
+++ tra.task/src/main/java/com/example/tra/task/EmployeeManagement.java
@@ -1,7 +1,14 @@
 package com.example.tra.task;
 
+import com.example.tra.task.Entities.Employee;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class EmployeeManagement {
     public static void main(String[] args) {
+
+        List<Employee> employees = new ArrayList<>();
 
     }
 }
