287a104fadf8e2097fc2683d60cad3c81ae683bb
Muneera Al Naabi <muneerasaifalnaabi@gmail.com>
2026-06-09 10:38:02 +0400

    Define relationships between entities where applicable using JPA annotations

---
+++ tra.task/src/main/java/com/example/tra/task/Interfaces/EmployeeRepository.java
@@ -0,0 +1,11 @@
+package com.example.tra.task.Interfaces;
+
+
+import com.example.tra.task.Entities.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EmployeeRepository extends JpaRepository<Employee,String> {
+
+}
