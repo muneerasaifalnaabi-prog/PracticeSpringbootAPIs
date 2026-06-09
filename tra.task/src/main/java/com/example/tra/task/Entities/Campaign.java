0c1fcbc4b6cd4076c611dbc0e8b16ad409cce609
Muneera Al Naabi <muneerasaifalnaabi@gmail.com>
2026-06-09 09:33:32 +0400

    Add and configure the required dependencies for Spring Data JPA

--- tra.task/src/main/java/com/example/tra/task/Entities/Campaign.java
+++ tra.task/src/main/java/com/example/tra/task/Entities/Campaign.java
@@ -1,6 +1,7 @@
 package com.example.tra.task.Entities;
 
 
+import jakarta.persistence.Entity;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -8,6 +9,7 @@ import lombok.NoArgsConstructor;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Entity
 public class Campaign {
     private String campaignId;
     private String campaignName;
--- tra.task/src/main/java/com/example/tra/task/Entities/Vehicle.java
+++ tra.task/src/main/java/com/example/tra/task/Entities/Vehicle.java
@@ -1,5 +1,9 @@
 package com.example.tra.task.Entities;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -7,7 +11,10 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 @Data
+@Entity
 public class Vehicle {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private String vehicleId;
     private String vehicleModel;
     private Integer rentalPricePerDay;
