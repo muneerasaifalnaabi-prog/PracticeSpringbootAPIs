9cc7669dd5d56835baea6f91dcac0109adb7e024
Muneera Al Naabi <muneerasaifalnaabi@gmail.com>
2026-06-09 10:42:49 +0400

    Define relationships between entities where applicable using JPA annotations

---
+++ tra.task/src/main/java/com/example/tra/task/Interfaces/CampaignRepository.java
@@ -0,0 +1,7 @@
+package com.example.tra.task.Interfaces;
+
+import com.example.tra.task.Entities.Campaign;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CampaignRepository extends JpaRepository<Campaign,String> {
+}
